36210984d930da0d60c52c6f7f2bd234a1a6718b
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-10-22 20:36:06 +0100

    created tutor and student apps, created database tables

--- schoolhub/settings.py
+++ schoolhub/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My apps
+    'student',
+    'tutor',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'schoolhub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+
+
+admin.site.register(models.Users, UsersAdmin)
