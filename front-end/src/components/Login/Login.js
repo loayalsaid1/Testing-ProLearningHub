0568293e23811ed8d720ad8b50ff288f272beb88
Loay Al-Said <l.programming555@gmail.com>
2024-10-26 00:30:14 +0300

    Use composeWihtDevTools from redux devtoosl extention

--- front-end/src/components/Login/Login.js
+++ front-end/src/components/Login/Login.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../../redux/actions/uiActionCreators';
 
 export default function Login() {
+	const dispatch = useDispatch();
+	const isLoading = useSelector((state) => state.ui.get('isLoading'));
+
+	function handleSubmit(event) {
+		event.preventDefault();
+
+		const email = event.target.email.value;
+		const password = event.target.password.value;
+
+		dispatch(login(email, password));
+	}
 
 	return <>
-	<form>
+	<form onSubmit={handleSubmit}>
 		<input name='email' id='email' type='text' placeholder='email' />
 		<input name='password' id='password' type='password' placeholder='password' />
-		<button type='submit'>Login</button>
+		<button type='submit' disabled={isLoading}>Login</button>
 	</form>
 	</>
 }
--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import rootReducer from './rootReducer';
 
+const storeEnhancer = composeWithDevTools(
+  applyMiddleware(thunk),
+)
 
 export default createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(applyMiddleware(thunk))
-    : applyMiddleware(thunk)
+  storeEnhancer
 );
-
-
