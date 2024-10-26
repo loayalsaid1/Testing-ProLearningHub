04b559c4245b40b6f24c365354bb99571d050e98
Loay Al-Said <l.programming555@gmail.com>
2024-10-26 21:11:55 +0300

    Add toastMiddleware to handle toasts based on actions

---
+++ front-end/src/redux/middlewares/toastMiddleware.js
@@ -0,0 +1,28 @@
+import toast from 'react-hot-toast';
+import * as actions from '../actions/uiActionTypes';
+
+const toastMiddleware = (store) => (next) => (action) => {
+	switch (action.type) {
+		case actions.SET_ERROR: {
+			const { errorMessage } = action.payload;
+			toast.error(errorMessage);
+			return next(action);
+		}
+
+		case actions.LOGIN_SUCCESS: {
+			toast.success('You are logged in!, Welcome!');
+			return next(action);
+		}
+
+		case actions.LOGOUT: {
+			toast.success('You are logged out!.. Bye!', {
+				icon: '👋'
+			});
+		}
+		default: {
+			return next(action);
+		}
+	}
+}
+
+export default toastMiddleware;
--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,13 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
 import { composeWithDevTools } from '@redux-devtools/extension';
+import toastMiddleware from './middlewares/toastMiddleware';
 import rootReducer from './rootReducer';
-
 const storeEnhancer = composeWithDevTools(
-  applyMiddleware(thunk),
-)
-
-export default createStore(
-  rootReducer,
-  storeEnhancer
+  applyMiddleware(thunk, toastMiddleware)
 );
+
+export default createStore(rootReducer, storeEnhancer);
