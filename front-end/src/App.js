89b449576b96eb4db499609550b7cfbb4e112b14
Loay Al-Said <l.programming555@gmail.com>
2024-10-26 05:08:15 +0300

    Add <Toaster /> to the app from react-hot-toast

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,34 +1,39 @@
 import React from 'react';
-import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
-import {toggleName } from './redux/actions/helloActionCreators'
+import { Toaster } from 'react-hot-toast';
+import './App.css';
+import { toggleName } from './redux/actions/helloActionCreators';
 import Login from './components/Login/Login';
 import { logout } from './redux/actions/uiActionCreators';
 import Spinner from './components/utilityComponents/Spinner';
 
 function App() {
   const name = useSelector((state) => state.hello.get('name'));
-  const isLoading = useSelector( state => state.ui.get('isLoading'));
-  const isLoggedIn = useSelector( state => state.ui.get('isLoggedIn'));
+  const isLoading = useSelector((state) => state.ui.get('isLoading'));
+  const isLoggedIn = useSelector((state) => state.ui.get('isLoggedIn'));
   const dispatch = useDispatch();
-
 
   return (
     <div className="APP">
-      { isLoading && <Spinner /> }
+      {isLoading && <Spinner />}
       <header className="App-header">
-        {!isLoggedIn 
-        ? <Login />
-        :(
-         <>
-          <p>Hello: {name}</p>
-          <button type='button' onClick={() => dispatch(toggleName())}>Toggle name</button>
-          <div>
-            <button type='button' onClick={() => dispatch(logout())}>Logout</button>
-          </div>
-         </>
-        )
-        }
+        {!isLoggedIn ? (
+          <Login />
+        ) : (
+          <>
+            <p>Hello: {name}</p>
+            <button type="button" onClick={() => dispatch(toggleName())}>
+              Toggle name
+            </button>
+            <div>
+              <button type="button" onClick={() => dispatch(logout())}>
+                Logout
+              </button>
+            </div>
+          </>
+        )}
+
+        <Toaster reverseOrder={true} />
       </header>
     </div>
   );
