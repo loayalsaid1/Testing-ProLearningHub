08777e23d05a0f4d245bca136942931973960e5b
Loay Al-Said <l.programming555@gmail.com>
2024-10-26 23:31:20 +0300

    Add google login logic in App and Login

--- front-end/src/components/Login/Login.js
+++ front-end/src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { formLogin, googleLogin } from '../../redux/actions/uiActionCreators';
+import { formLogin, googleLogin, loginFailure } from '../../redux/actions/uiActionCreators';
+import { GoogleLogin } from '@react-oauth/google';
 
 export default function Login() {
 	const dispatch = useDispatch();
@@ -15,11 +16,32 @@ export default function Login() {
 		dispatch(formLogin(email, password));
 	}
 
+	function handleGoogleLoginSuccess(response) {
+		dispatch(googleLogin(response.credential));
+	}
+
+	function handleGoogleLoginFailure(error) {
+		dispatch(loginFailure('Google login failure!'));
+		console.error(error);
+	}
+
 	return <>
 	<form onSubmit={handleSubmit}>
 		<input name='email' id='email' type='text' placeholder='email' />
 		<input name='password' id='password' type='password' placeholder='password' />
 		<button type='submit' disabled={isLoading}>Login</button>
 	</form>
+
+	<GoogleLogin
+  onSuccess={handleGoogleLoginSuccess}
+  onError={handleGoogleLoginFailure}
+  theme="filled_blue"
+  size="large"
+  shape="circle"
+  text="signin_with"
+	buttonText="Continue with Google"
+  logo_alignment="left"
+  width="300"                // Width of the button in pixels (optional)
+/>
 	</>
 }
