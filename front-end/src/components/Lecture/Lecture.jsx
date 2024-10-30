e9fcf880326164629120cb49bb97e889eb907e1e
Loay Al-Said <l.programming555@gmail.com>
2024-10-30 15:21:16 +0300

    Connect the lecture view to redux logic

--- front-end/src/components/Lecture/Lecture.jsx
+++ front-end/src/components/Lecture/Lecture.jsx
@@ -1,60 +1,92 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { extractVideoId } from '../../utils/utilFunctions';
+import { getLectureById } from '../../redux/actions/lecturesThunks';
 
+/**
+ * Next there must be away to keep the data in sync..
+ * may be just long polling.. or here setting a typestamp for when was last
+ * time fetch and with intervals.. check if data changed..
+ * or something outside the scope of redux.. which is utilizing websotckets
+ * to ping react when a change happens
+ */
 export default function Lecture({ lectureId }) {
-  const videoId = extractVideoId('https://youtu.be/F9-yqoS7b8w');
+  const lectureData = useSelector((state) =>
+    state.lectures.getIn(['lectures', lectureID])
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const videoId = extractVideoId(lectureData.get('videoLink'));
+    dispatch(getLectureById(lectureId));
+  }, [dispatch, lectureId, lectureData]);
+
+  const demos = lectureData
+    .get('demos')
+    .map((demo, index) => (
+      <li key={index}>
+        <a href={demo.url}>{demo.title}</a>
+      </li>
+    ))
+    .toJS();
+  const shorts = lectureData
+    .get('shorts')
+    .map((short, index) => (
+      <li key={index}>
+        <a href={short.url}>{short.title}</a>
+      </li>
+    ))
+    .toJS();
+  const quizzez = lectureData
+    .get('quizzez')
+    .map((quizzez, index) => (
+      <li key={index}>
+        <a href={quizzez.url}>{quizzez.title}</a>
+      </li>
+    ))
+    .toJS();
+
   return (
     <>
-      <h1>Week 4 Memory
-      </h1>
-      <p>
-      Pointers. Segmentation Faults. Dynamic Memory Allocation. Stack. Heap. Buffer Overflow. File I/O. Images.
-
-{' '}
-      </p>
-      <iframe
-        width="800px"
-        height="500px"
-        src={`https://www.youtube.com/embed/${videoId}`}
-      ></iframe>
+      <h1>{lectureData.title}</h1>
+      <p>{lectureData.description}</p>
+      <iframe src={`https://www.youtube.com/embed/${videoId}`}></iframe>
 
       <details>
         <summary>Lecture</summary>
         <ul>
-          <li><a>Audio</a></li>
-          <li><a>Notes</a></li>
-          <li><a>Video</a></li>
-          <li><a>Slides</a></li>
+          <li>
+            <a href={lectureData.get('audioLink')}>Audio</a>
+          </li>
+          <li>
+            <a href={lectureData.get('Notes')}>Notes</a>
+          </li>
+          //{' '}
+          <li>
+            <a href={lectureData.get('')}>Video</a>
+          </li>
+          <li>
+            <a href={lectureData.get('slides')}>Slides</a>
+          </li>
           <details>
             <summary>Demos</summary>
-            <ol>
-              <li><a>Demo 1</a></li>
-              <li><a>Demo 2</a></li>
-              <li><a>Demo 3</a></li>
-            </ol>
+            {demos.length === 0 ? <p>No Demos</p> : <ol>{demos}</ol>}
           </details>
-          <li><a>transcript</a></li>
-          <li><a>subtitles</a></li>
+          <li>
+            <a>transcript</a>
+          </li>
+          <li>
+            <a>subtitles</a>
+          </li>
         </ul>
       </details>
       <details>
         <summary>Shorts & Extras</summary>
-        <ol>
-          <li><a>Hexadecimal</a></li>
-          <li><a>Pointers</a></li>
-          <li><a>Defining Custom Types</a></li>
-          <li><a>Dynamic Memory Allocation</a></li>
-          <li><a>Call Stacks</a></li>
-          <li><a>File Pointers</a></li>
-        </ol>
+        {shorts.length === 0 ? <p>No Shorts</p> : <ol>{shorts}</ol>}
       </details>
       <details>
         <summary>Quizzez & Problem Set</summary>
-        <ol>
-          <li><a>Lab 1</a></li>
-          <li><a>Lab 2</a></li>
-          <li><a>Lectur Problemset</a></li>
-        </ol>
+        {quizzez.length === 0 ? <p>No Quizzez</p> : <ol>{quizzez}</ol>}
       </details>
     </>
   );
