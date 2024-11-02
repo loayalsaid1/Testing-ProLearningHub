201bfcccff386f34e3f531c392c21bc844e42c4d
Loay Al-Said <l.programming555@gmail.com>
2024-11-03 00:33:06 +0200

    Set up discussionsReducer with initalState

---
+++ front-end/src/redux/reducers/discussionsReducer.js
@@ -0,0 +1,15 @@
+import { fromJS } from 'immutable';
+
+export const initialState = fromJS({
+  lecturesDiscussions: {},
+  isLoading: false,
+  discussionsError: null,
+});
+
+export const discussionsReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+};
