19edd3e24de511215cbfc0242fcb580a7079ff47
Loay Al-Said <l.programming555@gmail.com>
2024-11-02 23:14:27 +0200

    Use DiscussionEntry in LectureDiscussion [ mock data for now]

--- front-end/src/components/Discussion/LectureDiscussion.jsx
+++ front-end/src/components/Discussion/LectureDiscussion.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import { fromJS } from 'immutable';
 import SearchField from '../sharedComponents/SearchField';
 import DiscussionEntryEditor from './DiscussionEntryEditor';
+import DiscussionEntry from './DiscussionEntry';
 
 export default function LectureDiscussion({ lectureId = '' }) {
   const [askNewQuestion, setAskNewQuestion] = useState(false);
@@ -18,12 +20,11 @@ export default function LectureDiscussion({ lectureId = '' }) {
       <h2>Lecture Discussion</h2>
       <SearchField placeholder="Search lecture questions" />
       <div>
-        <h3>Question Entry here</h3>
-        <h3>Question Entry here</h3>
-        <h3>Question Entry here</h3>
-        <h3>Question Entry here</h3>
-        <h3>Question Entry here</h3>
-        <h3>Question Entry here</h3>
+        {
+          mockLectureDiscussionEntries.map(entry => {
+            return <DiscussionEntry key={entry.id} content={entry} />
+          })
+        }
       </div>
       {/* Depeneding on there is more or not */}
       <button type="button">See more</button>
@@ -38,3 +39,203 @@ export default function LectureDiscussion({ lectureId = '' }) {
     </div>
   );
 }
+
+
+/**
+ * lectureDiscussionEntryies
+ * [
+	{
+		user: { pictureThubmnail, name}
+		updateDate / creation data .....  or updatedAt
+		title
+		upvotes [number]
+    upvoted [boolean]
+		repliesCount [number]
+		id ........ of couse.. incase i forgot it in any previous reponses.. it must be there for everything
+	}
+	...
+]
+ */
+
+const mockLectureDiscussionEntries = fromJS([
+  {
+    id: '1',
+    title: 'How does react work?',
+    user: {
+      name: 'John Doe',
+      pictureThumbnail: 'https://picsum.photos/200/300',
+    },
+    updatedAt: '2022-01-01T00:00:00.000Z',
+    upvotes: 100,
+    upvoted: false,
+    repliesCount: 20,
+  },
+  {
+    id: '2',
+    title: 'How does react state work?',
+    user: {
+      name: 'Jane Doe',
+      pictureThumbnail: 'https://picsum.photos/200/301',
+    },
+    updatedAt: '2024-11-02T07:00:00.000Z',
+    upvotes: 50,
+    upvoted: true,
+    repliesCount: 10,
+  },
+  {
+    id: '3',
+    title: 'How does react context work?',
+    user: {
+      name: 'John Doe',
+      pictureThumbnail: 'https://picsum.photos/200/302',
+    },
+    updatedAt: '2022-01-03T00:00:00.000Z',
+    upvotes: 30,
+    upvoted: false,
+    repliesCount: 5,
+  },
+  {
+    id: '2',
+    title: 'How does react state work?',
+    user: {
+      name: 'Jane Doe',
+      pictureThumbnail: 'https://picsum.photos/200/301',
+    },
+    updatedAt: '2024-11-02T07:00:00.000Z',
+    upvotes: 50,
+    upvoted: true,
+    repliesCount: 10,
+  },
+  {
+    id: '3',
+    title: 'How does react context work?',
+    user: {
+      name: 'John Doe',
+      pictureThumbnail: 'https://picsum.photos/200/302',
+    },
+    updatedAt: '2022-01-03T00:00:00.000Z',
+    upvotes: 30,
+    upvoted: false,
+    repliesCount: 5,
+  },
+  {
+    id: '2',
+    title: 'How does react state work?',
+    user: {
+      name: 'Jane Doe',
+      pictureThumbnail: 'https://picsum.photos/200/301',
+    },
+    updatedAt: '2024-11-02T07:00:00.000Z',
+    upvotes: 50,
+    upvoted: true,
+    repliesCount: 10,
+  },
+  {
+    id: '3',
+    title: 'How does react context work?',
+    user: {
+      name: 'John Doe',
+      pictureThumbnail: 'https://picsum.photos/200/302',
+    },
+    updatedAt: '2022-01-03T00:00:00.000Z',
+    upvotes: 30,
+    upvoted: false,
+    repliesCount: 5,
+  },
+  {
+    id: '2',
+    title: 'How does react state work?',
+    user: {
+      name: 'Jane Doe',
+      pictureThumbnail: 'https://picsum.photos/200/301',
+    },
+    updatedAt: '2024-11-02T07:00:00.000Z',
+    upvotes: 50,
+    upvoted: true,
+    repliesCount: 10,
+  },
+  {
+    id: '3',
+    title: 'How does react context work?',
+    user: {
+      name: 'John Doe',
+      pictureThumbnail: 'https://picsum.photos/200/302',
+    },
+    updatedAt: '2022-01-03T00:00:00.000Z',
+    upvotes: 30,
+    upvoted: false,
+    repliesCount: 5,
+  },
+  {
+    id: '2',
+    title: 'How does react state work?',
+    user: {
+      name: 'Jane Doe',
+      pictureThumbnail: 'https://picsum.photos/200/301',
+    },
+    updatedAt: '2024-11-02T07:00:00.000Z',
+    upvotes: 50,
+    upvoted: true,
+    repliesCount: 10,
+  },
+  {
+    id: '3',
+    title: 'How does react context work?',
+    user: {
+      name: 'John Doe',
+      pictureThumbnail: 'https://picsum.photos/200/302',
+    },
+    updatedAt: '2022-01-03T00:00:00.000Z',
+    upvotes: 30,
+    upvoted: false,
+    repliesCount: 5,
+  },
+  {
+    id: '2',
+    title: 'How does react state work?',
+    user: {
+      name: 'Jane Doe',
+      pictureThumbnail: 'https://picsum.photos/200/301',
+    },
+    updatedAt: '2024-11-02T07:00:00.000Z',
+    upvotes: 50,
+    upvoted: true,
+    repliesCount: 10,
+  },
+  {
+    id: '3',
+    title: 'How does react context work?',
+    user: {
+      name: 'John Doe',
+      pictureThumbnail: 'https://picsum.photos/200/302',
+    },
+    updatedAt: '2022-01-03T00:00:00.000Z',
+    upvotes: 30,
+    upvoted: false,
+    repliesCount: 5,
+  },
+  {
+    id: '2',
+    title: 'How does react state work?',
+    user: {
+      name: 'Jane Doe',
+      pictureThumbnail: 'https://picsum.photos/200/301',
+    },
+    updatedAt: '2024-11-02T07:00:00.000Z',
+    upvotes: 50,
+    upvoted: true,
+    repliesCount: 10,
+  },
+  {
+    id: '3',
+    title: 'How does react context work?',
+    user: {
+      name: 'John Doe',
+      pictureThumbnail: 'https://picsum.photos/200/302',
+    },
+    updatedAt: '2022-01-03T00:00:00.000Z',
+    upvotes: 30,
+    upvoted: false,
+    repliesCount: 5,
+  },
+]);
