8c18ae1fc5436c633a34c56f201b7d73bea8e10e
Loay Al-Said <l.programming555@gmail.com>
2024-11-02 23:13:08 +0200

    Upload files on submit in the editor

--- front-end/src/components/Discussion/DiscussionEntryEditor.jsx
+++ front-end/src/components/Discussion/DiscussionEntryEditor.jsx
@@ -1,23 +1,63 @@
 import React, {useState} from 'react';
+import { useDispatch } from 'react-redux';
+import ImageKit from "imagekit-javascript";
 import TextEditor from '../TextEditor/TextEditor';
+import { clearError, setError, toggleLoading } from '../../redux/actions/uiActionCreators';
+
+
+const imagekit = new ImageKit({
+  publicKey: "public_tTc9vCi5O7L8WVAQquK6vQWNx08=",
+  urlEndpoint: "https://ik.imagekit.io/loayalsaid1/proLearningHub",
+});
 
 export default function DiscussionEntryEditor({ onPublish }) {
 	const [details, setDetails] = useState('');
 	const [files, setFiles] = useState([]);
 	const editorPlaceholder = 'Optionally.. Add elaboration to your question if you need to';
+	const dispatch = useDispatch();
 
-	// That would be the setting the dtails with the correct urls not the temp ones
-	const uplaodFiles = () => {setDetails(details)}
-	const handleSubmit = (e) => {
+	const replaceTempImageUrls = async () => {
+		let newDetails = details;
+
+		for (const { file, fileUrl } of files) {
+			try {
+				const authParamsResponse = await fetch('http://localhost:3000/auth/imagekit');
+				if (!authParamsResponse.ok) {
+					throw new Error('Failed to fetch authentication parameters');
+
+				}
+				const authParams = await authParamsResponse.json();
+				if (!authParams) {
+					throw new Error('Invalid authentication parameters');
+				}
+
+				const uploadResponse = await imagekit.upload({
+					file,
+					fileName: `LectureDiscussionEntry_${Date.now()}`,
+					
+					...authParams,
+				});
+
+				newDetails = newDetails.replace(fileUrl, uploadResponse.url);
+			} catch (error) {
+				dispatch(setError('discussion', `Error uploading file: ${error.message}`));
+			}
+		}
+
+		return newDetails;
+	};
+
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-		/**
-		 * upload images and make the content ready
-		 * 
-		 * call the publish thing passed as props
-		 */
-		// Upload images 
+		dispatch(toggleLoading())
+
+		const newDetails = await replaceTempImageUrls()
+
+		dispatch(toggleLoading())
+		dispatch(clearError('discussion'))
+		
 		const title = e.target.elements.title.value;
-		onPublish(title, details)
+		onPublish(title, newDetails);
 	}
 
 	return (
