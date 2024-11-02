fbdb43748e05aec624917de4573ed5c5722e1441
timmySpark <timmyspark1@gmail.com>
2024-11-02 03:12:43 +0100

    [feat] :- refined code, added commits and removed un-used codes

--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+''' Students Endpoint and Urls '''
+
 urlpatterns = [
     path('resources/<int:course_id>', views.resources, name='resources'),
     path('courses/', views.courses, name='courses'),
