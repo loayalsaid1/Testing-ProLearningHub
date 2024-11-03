fc733b18f9dcd2c2e897724b72d4391d96f2c14d
Loay Al-Said <l.programming555@gmail.com>
2024-11-03 06:16:32 +0200

    Add discussions slice to root reducer

--- front-end/src/redux/rootReducer.js
+++ front-end/src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import helloReducer from './reducers/helloReducer';
 import uiReducer from './reducers/uiReducer';
 import lecturesReducer from './reducers/lecturesReducer';
+import discussionsReducer from './reducers/discussionsReducer';
 
 const rootReducer = combineReducers({
 	hello: helloReducer,
 	ui: uiReducer,
 	lectures: lecturesReducer,
+	discussions: discussionsReducer
 })
 
 export default rootReducer;
