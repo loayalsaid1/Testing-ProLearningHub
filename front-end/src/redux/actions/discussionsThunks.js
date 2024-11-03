e6f29a1c4e97454cec7494cebe79aed15f5035be
Loay Al-Said <l.programming555@gmail.com>
2024-11-03 17:46:52 +0200

    Add discusssionsTHunk

--- front-end/src/redux/actions/discussionsThunks.js
+++ front-end/src/redux/actions/discussionsThunks.js
@@ -20,6 +20,6 @@ export const getLectureDiscussions = (lectureId) => async (dispatch) => {
 		
 	}	catch (error) {
 		console.error(error.message);
-		dispatch(discussionsActions.lectureDiscussionFailure(data.message));
+		dispatch(discussionsActions.lectureDiscussionFailure(error.message));
 	}
 }
