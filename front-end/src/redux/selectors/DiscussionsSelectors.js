47ed24b55c00fd45c5972a08556ca10f5d70d717
Loay Al-Said <l.programming555@gmail.com>
2024-11-04 21:35:34 +0200

    Add selectCourseGeneralDiscussions seelctor

--- front-end/src/redux/selectors/DiscussionsSelectors.js
+++ front-end/src/redux/selectors/DiscussionsSelectors.js
@@ -1,11 +1,15 @@
 import { createSelector } from 'reselect';
 
-export const selectDiscussionsIsLoading = (state) => state.discussions.get('isLoading');
+export const selectDiscussionsIsLoading = (state) =>
+  state.discussions.get('isLoading');
 
-const selectLecturesDiscussions = (state) => state.discussions.get('lecturesDiscussions');
+const selectLecturesDiscussions = (state) =>
+  state.discussions.get('lecturesDiscussions');
 
 export const makeLectureDiscussionsSelector = (lectureId) =>
-	createSelector([selectLecturesDiscussions], (lecturesDiscussions) =>
-		lecturesDiscussions.get(lectureId)
-	);
+  createSelector([selectLecturesDiscussions], (lecturesDiscussions) =>
+    lecturesDiscussions.get(lectureId)
+  );
 
+export const selectCourseGeneralDiscussion = (state) =>
+  state.discussions.get('courseGeneralDiscussion');
