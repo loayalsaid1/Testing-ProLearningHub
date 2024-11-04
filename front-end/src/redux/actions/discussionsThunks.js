73c289e5f2717dabf64c958d55e90bd5e01cfb4b
Loay Al-Said <l.programming555@gmail.com>
2024-11-04 09:27:36 +0200

    Add AddLectureDiscussionEntry thunk

--- front-end/src/redux/actions/discussionsThunks.js
+++ front-end/src/redux/actions/discussionsThunks.js
@@ -1,3 +1,4 @@
+import toast from 'react-hot-toast';
 import * as discussionsActions from './discussionsActionCreators';
 
 
@@ -20,5 +21,45 @@ export const getLectureDiscussions = (lectureId) => async (dispatch) => {
 	}	catch (error) {
 		console.error(error.message);
 		dispatch(discussionsActions.lectureDiscussionFailure(`Error fetching entries: ${error.message}`));
+	}
+}
+
+export const AddLectureDiscussionEntry = (lectureId, title, details) => async (dispatch) => {
+	dispatch(discussionsActions.addDiscussionEntryRequest());
+
+	const userId = getState().ui.getIn(['user', 'id']) || 'testId';
+	const promise = toast.promise(
+		fetch(`${DOMAIN}/lectures/${lectureId}/discussion`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				userId,
+				title,
+				body: details
+			})
+		}),
+		{
+			loading: 'Sending your Entry',
+			success: 'Your Entry has been sent',
+			error: 'Error sending your question',
+		}
+	)
+
+	try {
+		const response = await promise;
+		const data = await response.json();
+
+		if (!response.ok) {
+			throw new Error(data.message);
+		}
+		dispatch(discussionsActions.addDiscussionEntrySuccess({
+			lectureId,
+			entry: data,
+		}));
+	} catch (error) {
+		console.error(error.message);
+		dispatch(discussionsActions.addDiscussionEntryFailure(`Error adding entry: ${error.message}`));
 	}
 }
