e96bc2888833b8e15c473cf8966dd623b738f672
Loay Al-Said <l.programming555@gmail.com>
2024-11-05 03:37:09 +0200

    Add ReplyEntry component

--- front-end/src/components/Replies/Replies.jsx
+++ front-end/src/components/Replies/Replies.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { CircleArrowUp, Dot, EllipsisVertical } from 'lucide-react';
+import ReplyEntry from './ReplyEntry';
 import TextEditor from '../TextEditor/TextEditor';
+import { CircleArrowUp, Dot, EllipsisVertical } from 'lucide-react';
+import {fromJS} from 'immutable';
 
 export default function Replies() {
   const [showQuestionEditor, setShowQuestionEditor] = useState(false);
@@ -37,91 +39,14 @@ export default function Replies() {
 
       {/* Replies part */}
       <div>
-        <p>3 Replies</p>
+        <p>{mockReplies.size} repl{mockReplies.size !== 1 ? 'ies' : 'y'}</p>
         {/* Replie entry */}
         <div>
-          <div>
-            <img
-              src="https://picsum.photos/50"
-              width="50"
-              height="50"
-              alt="Replier's image"
-            />
-          </div>
-          <div>
-            <p>Replier's Name</p>
-            <p>X days ago</p>
-
-            <div>
-              Question Text Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Fugiat, porro. Dolorem labore aut dolor omnis.
-            </div>
-          </div>
-          {/* side buttons */}
-          <div>
-            <button type="button">
-              12 <CircleArrowUp />
-            </button>
-            <button type="button">
-              <EllipsisVertical />
-            </button>
-          </div>
-        </div>
-        <div>
-          <div>
-            <img
-              src="https://picsum.photos/50"
-              width="50"
-              height="50"
-              alt="Replier's image"
-            />
-          </div>
-          <div>
-            <p>Replier's Name</p>
-            <p>X days ago</p>
-
-            <div>
-              Question Text Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Fugiat, porro. Dolorem labore aut dolor omnis.
-            </div>
-          </div>
-          {/* side buttons */}
-          <div>
-            <button type="button">
-              12 <CircleArrowUp />
-            </button>
-            <button type="button">
-              <EllipsisVertical />
-            </button>
-          </div>
-        </div>
-        <div>
-          <div>
-            <img
-              src="https://picsum.photos/50"
-              width="50"
-              height="50"
-              alt="Replier's image"
-            />
-          </div>
-          <div>
-            <p>Replier's Name</p>
-            <p>X days ago</p>
-
-            <div>
-              Question Text Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Fugiat, porro. Dolorem labore aut dolor omnis.
-            </div>
-          </div>
-          {/* side buttons */}
-          <div>
-            <button type="button">
-              12 <CircleArrowUp />
-            </button>
-            <button type="button">
-              <EllipsisVertical />
-            </button>
-          </div>
+          {
+            mockReplies.map(
+              (reply) => <ReplyEntry key={reply.get('id')} content={reply} />
+            )
+          }
         </div>
       </div>
 
@@ -142,3 +67,53 @@ export default function Replies() {
     </>
   );
 }
+
+
+const question = fromJS({
+  id: 'question-1',
+  title: 'How does react hooks work?',
+  user: {
+    name: 'John Doe',
+    pictureThumbnail: 'https://picsum.photos/100',
+  },
+  updatedAt: '2022-01-01T00:00:00.000Z',
+  upvotes: 100,
+  upvoted: false,
+  repliesCount: 20,
+});
+
+const mockReplies = fromJS([
+  {
+    id: 'reply-1',
+    user: {
+      name: 'Jane Doe',
+      pictureThumbnail: 'https://picsum.photos/100',
+    },
+    updatedAt: '2024-11-02T07:00:00.000Z',
+    upvotes: 50,
+    upvoted: true,
+    body: 'How does react hooks work?',
+  },
+  {
+    id: 'reply-2',
+    user: {
+      name: 'John Doe',
+      pictureThumbnail: 'https://picsum.photos/100',
+    },
+    updatedAt: '2022-01-03T00:00:00.000Z',
+    upvotes: 30,
+    upvoted: false,
+    body: 'How does react context work?',
+  },
+  {
+    id: 'reply-3',
+    user: {
+      name: 'Jane Doe',
+      pictureThumbnail: 'https://picsum.photos/100',
+    },
+    updatedAt: '2024-11-04T07:00:00.000Z',
+    upvotes: 20,
+    upvoted: false,
+    body: 'How does react hooks work?',
+  },
+]);
