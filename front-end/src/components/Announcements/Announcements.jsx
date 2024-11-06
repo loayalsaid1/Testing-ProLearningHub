d5a4f548415ff314df762da48f89a938f6ed8671
Loay Al-Said <l.programming555@gmail.com>
2024-11-06 20:37:58 +0200

    Connect Announcements to Redux properly for fetching

--- front-end/src/components/Announcements/Announcements.jsx
+++ front-end/src/components/Announcements/Announcements.jsx
@@ -1,11 +1,21 @@
-import React, {useState} from 'react';
+import React, {useEffect} from 'react';
 import Loading from '../utilityComponents/Loading';
 import AnnouncementEntry from './AnnouncementEntry';
-import {fromJS} from 'immutable'
-import { useSelector } from 'react-redux';
-import { selectIsLoading } from '../../redux/selectors/announcementsSelectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsLoading, selectAnnouncements } from '../../redux/selectors/announcementsSelectors';
+import { fetchAnnouncements } from '../../redux/actions/announcementsThunks';
+
 export default function Announcements() {
 		const isLoading = useSelector(selectIsLoading);
+		const announcements = useSelector(selectAnnouncements);
+
+		const dispatch = useDispatch();
+		useEffect(() => {
+			if (!announcements?.size)
+				dispatch(fetchAnnouncements());
+		}, [dispatch, announcements])
+
+
 		return (
 			<div>
 			<h1>Announcements</h1>
@@ -25,29 +35,3 @@ export default function Announcements() {
 		</div>
 	);
 }
-	const announcements = fromJS([
-		{
-			user: {
-				name: 'John Doe',
-				id: '1234567890',
-				pictureThumbnail: 'https://picsum.photos/100',
-			},
-			id: 'announcement-1',
-			title: 'This is the title of the announcement',
-			body: 'This is the content of the announcement',
-			commentsCount: 10,
-			updatedAt: '2022-01-01T00:00:00.000Z',
-		},
-		{
-			user: {
-				name: 'Jane Doe',
-				id: '9876543210',
-				pictureThumbnail: 'https://picsum.photos/101',
-			},
-			id: 'announcement-2',
-			title: 'This is the title of the second announcement',
-			body: 'This is the content of the second announcement',
-			commentsCount: 5,
-			updatedAt: '2024-11-01T00:00:00.000Z',
-		},
-	]);
