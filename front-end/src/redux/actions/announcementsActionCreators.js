d4d576f73c63ca5f63cd9a6fdb27d4f57facc6a9
Loay Al-Said <l.programming555@gmail.com>
2024-11-06 20:44:53 +0200

    Add Comments action creators

--- front-end/src/redux/actions/announcementsActionCreators.js
+++ front-end/src/redux/actions/announcementsActionCreators.js
@@ -26,3 +26,17 @@ export const fetchAnnouncementsSuccess = (data) => ({
   type: actions.FETCH_ANNOUNCEMENTS_SUCCESS,
   payload: { data },
 });
+
+export const fetchAnnouncementCommentsRequest = (announcementId) => ({
+  type: actions.FETCH_ANNOUNCEMENT_COMMENTS_REQUEST,
+});
+
+export const fetchAnnouncementCommentsFailure = (errorMessage) => ({
+  type: actions.FETCH_ANNOUNCEMENT_COMMENTS_FAILURE,
+  payload: { errorMessage },
+});
+
+export const fetchAnnouncementCommentsSuccess = (announcementId, data) => ({
+  type: actions.FETCH_ANNOUNCEMENT_COMMENTS_SUCCESS,
+  payload: { announcementId, comments: data },
+});
