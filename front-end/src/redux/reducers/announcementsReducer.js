ebee345c84b74e062ec4ae54595de3a98d62feb2
Loay Al-Said <l.programming555@gmail.com>
2024-11-06 19:37:18 +0200

    Add handle the initial actions in the reducer

--- front-end/src/redux/reducers/announcementsReducer.js
+++ front-end/src/redux/reducers/announcementsReducer.js
@@ -1,4 +1,5 @@
 import { fromJS } from 'immutable';
+import * as actions from '../actions/announcementsActionTypes';
 
 export const initialState = fromJS({
   isLoading: false,
@@ -7,10 +8,37 @@ export const initialState = fromJS({
   comments: {},
 });
 
-export default function announcementsReducer(state = initialState, action = {}) {
-	switch (action.type) {
-		default:
-			return state;
-	}
-}
+export default function announcementsReducer(
+  state = initialState,
+  action = {}
+) {
+  switch (action.type) {
+    case actions.TOGGLE_ANNOUNCEMENTS_LOADING:
+      return state.set('isLoading', !state.get('isLoading'));
 
+    case actions.SET_ANNOUNCEMENTS_ERROR:
+      return state.set('announcementsError', action.payload.errorMessage);
+
+    case actions.CLEAR_ANNOUNCEMENTS_ERROR:
+      return state.set('announcementsError', null);
+
+    case actions.FETCH_ANNOUNCEMENTS_REQUEST:
+      return state.set('isLoading', true);
+
+    case actions.FETCH_ANNOUNCEMENTS_FAILURE:
+      return state.merge({
+        isLoading: false,
+        announcementsError: action.payload.errorMessage,
+      });
+
+    case actions.FETCH_ANNOUNCEMENTS_SUCCESS:
+      return state.merge({
+        isLoading: false,
+        announcementsError: null,
+        announcements: fromJS(action.payload.data),
+      });
+
+    default:
+      return state;
+  }
+}
