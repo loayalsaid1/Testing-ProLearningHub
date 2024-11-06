24b4ae799f7ced692fe3c35f3f37affebe475b8c
Loay Al-Said <l.programming555@gmail.com>
2024-11-06 18:39:07 +0200

    Add loading placeholder for Announcements

--- front-end/src/components/Announcements/Announcements.jsx
+++ front-end/src/components/Announcements/Announcements.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Loading from '../utilityComponents/Loading';
 import AnnouncementEntry from './AnnouncementEntry';
 import {fromJS} from 'immutable'
 export default function Announcements() {
-
+		const [isLoading, setIsLoading] = useState(true);
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 500);
 
 		return (
 			<div>
 			<h1>Announcements</h1>
-			{announcements.map((announcement) => (
-				<AnnouncementEntry key={announcement.get('id')} content={announcement} />
-			))}
+			{
+				isLoading ? (
+					<Loading />
+				) : (
+					announcements.map((announcement) => (
+						<AnnouncementEntry key={announcement.get('id')} content={announcement} />
+					))
+				)
+			}
 		</div>
 	);
 }
