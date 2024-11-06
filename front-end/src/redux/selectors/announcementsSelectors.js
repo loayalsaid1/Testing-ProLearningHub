fa014878e2f8c1ba70c0993b0314f284d4309e78
Loay Al-Said <l.programming555@gmail.com>
2024-11-06 20:55:49 +0200

    Add fethcComments selectors

--- front-end/src/redux/selectors/announcementsSelectors.js
+++ front-end/src/redux/selectors/announcementsSelectors.js
@@ -1,2 +1,11 @@
+import { createSelector } from 'reselect';
+
+
 export const selectIsLoading = (state) => state.announcements.get('isLoading');
 export const selectAnnouncements = (state) => state.announcements.get('announcements');
+
+export const selectComments = (state) => state.announcements.get('comments');
+export const makeCommentsSelector = (announcementId) => createSelector(
+	selectComments,
+	(comments) => comments.get(announcementId)
+)
