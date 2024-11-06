f4a1a800a7bc906b892afce96893fe7487582615
Loay Al-Said <l.programming555@gmail.com>
2024-11-06 23:43:29 +0200

    Connect comments List to redux

--- front-end/src/components/Announcements/CommentsList.jsx
+++ front-end/src/components/Announcements/CommentsList.jsx
@@ -1,100 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  selectIsCommentsLoading,
+  makeCommentsSelector,
+} from '../../redux/selectors/announcementsSelectors';
 import Loading from '../utilityComponents/Loading';
 import CommentEntry from './CommentEntry';
 import { fromJS } from 'immutable';
+import { fetchAnnouncementComments } from '../../redux/actions/announcementsThunks';
 
 export default function CommentsList({ announcementId = 'testId' }) {
   const [limit, setLimit] = useState(10);
-  const [isLoading, setIsLoading] = useState(true);
-  setTimeout(() => {
-    setIsLoading(false);
-  });
+  const isLoading = useSelector(selectIsCommentsLoading);
+  const selectComments = makeCommentsSelector(announcementId);
+  const comments = useSelector(selectComments);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!comments || ! comments.size)
+      dispatch(fetchAnnouncementComments(announcementId));
+  }, [dispatch, comments, announcementId]);
 
   return (
     <div>
       {isLoading ? (
         <Loading />
+      ) : !comments || !comments.size ? (
+        <h3>No comments yet</h3>
       ) : (
-
-        mockComments
-          .slice(0, limit)
-          .map((comment) => (
+        <>
+          {comments.slice(0, limit).map((comment) => (
             <CommentEntry key={comment.get('id')} content={comment} />
-          ))
-      )}
-      {limit < mockComments.size && (
-        <button type="button" onClick={() => setLimit(limit + 10)}>
-          Show more
-        </button>
-      )}
-      {limit > 10 && (
-        <button type="button" onClick={() => setLimit(limit - 10)}>
-          Show less
-        </button>
+          ))}
+          {limit < comments.size && (
+            <button type="button" onClick={() => setLimit(limit + 10)}>
+              Show more
+            </button>
+          )}
+          {limit > 10 && (
+            <button type="button" onClick={() => setLimit(limit - 10)}>
+              Show less
+            </button>
+          )}
+        </>
       )}
     </div>
   );
 }
-
-const mockComments = fromJS([
-  {
-    id: 'comment-1',
-    announcementId: 'testId',
-    user: {
-      name: 'John Doe',
-      pictureThumbnail: 'https://picsum.photos/100',
-    },
-    updatedAt: '2024-11-02T07:00:00.000Z',
-    upvotes: 50,
-    upvoted: true,
-    body: 'How does react hooks work?',
-  },
-  {
-    id: 'comment-2',
-    announcementId: 'testId',
-    user: {
-      name: 'Jane Doe',
-      pictureThumbnail: 'https://picsum.photos/101',
-    },
-    updatedAt: '2022-01-03T00:00:00.000Z',
-    upvotes: 30,
-    upvoted: false,
-    body: 'How does react context work?',
-  },
-  {
-    id: 'comment-3',
-    announcementId: 'testId',
-    user: {
-      name: 'John Doe',
-      pictureThumbnail: 'https://picsum.photos/102',
-    },
-    updatedAt: '2024-11-04T07:00:00.000Z',
-    upvotes: 20,
-    upvoted: false,
-    body: 'How does react useState work?',
-  },
-  {
-    id: 'comment-4',
-    announcementId: 'testId',
-    user: {
-      name: 'Jane Doe',
-      pictureThumbnail: 'https://picsum.photos/103',
-    },
-    updatedAt: '2022-01-05T00:00:00.000Z',
-    upvotes: 10,
-    upvoted: true,
-    body: 'How does react hooks work?',
-  },
-  {
-    id: 'comment-5',
-    announcementId: 'testId',
-    user: {
-      name: 'John Doe',
-      pictureThumbnail: 'https://picsum.photos/104',
-    },
-    updatedAt: '2024-11-06T07:00:00.000Z',
-    upvotes: 5,
-    upvoted: false,
-    body: 'How does react useState work?',
-  },
-]);
