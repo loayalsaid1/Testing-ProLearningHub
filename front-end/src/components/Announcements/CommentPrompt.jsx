2c928fac2abea4c571df0871bffed600a3d69e2c
Loay Al-Said <l.programming555@gmail.com>
2024-11-06 13:53:06 +0200

    Create teh CommentPrompt component

--- front-end/src/components/Announcements/CommentPrompt.jsx
+++ front-end/src/components/Announcements/CommentPrompt.jsx
@@ -1,2 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
+export default function CommentPrompt() {
+  const userPicture =
+    useSelector((state) => state.ui.getIn(['user', 'pictureThumbnail'])) ||
+    'https://picsum.photos/100';
+  function submitComment(commentText) {
+    console.log(commentText);
+  }
+  return (
+    <div>
+      <img src={userPicture} alt="user" />
+      <StandAloneComment
+        onSubmit={submitComment}
+        placeholder="Write your comment here..."
+      />
+    </div>
+  );
+}
