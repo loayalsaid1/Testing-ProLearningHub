2c9ce126dfd435e7010d7c03e64a3a4504057c8d
Loay Al-Said <l.programming555@gmail.com>
2024-11-07 14:25:30 +0200

    Playig with routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
+import { Routes, Route, Navigate, Outlet, Link } from 'react-router-dom';
 import './App.css';
 import { toggleName } from './redux/actions/helloActionCreators';
 import { logout } from './redux/actions/uiActionCreators';
 import Spinner from './components/utilityComponents/Spinner';
 import { googleLogout } from '@react-oauth/google';
 import Authintication from './components/Authintication/Authintication';
+import Lectures from './components/Lectures/Lectures';
+import Announcements from './components/Announcements/Announcements'
+import GeneralDiscussion from './components/GeneralDiscussion/GeneralDiscussion';
+import Lecture from './components/Lecture/Lecture';
+import Replies from './components/Replies/Replies';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+
+
+function ProtectedLayout() {
+  const isLoggedIn = useSelector((state) => state.ui.get('isLoggedIn'));
+  // const isLoggedIn = true;
+//
+  return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
+}
 
 function App() {
   const name = useSelector((state) => state.hello.get('name'));
   const isLoading = useSelector((state) => state.ui.get('isLoading'));
-  const isLoggedIn = useSelector((state) => state.ui.get('isLoggedIn'));
   const dispatch = useDispatch();
 
   const handleLogout = () => {
@@ -19,28 +34,37 @@ function App() {
     googleLogout();
   };
 
-  
 
   return (
     <div className="APP">
       {isLoading && <Spinner />}
       <header className="App-header">
-        {!isLoggedIn ? (
-          <Authintication />
-        ) : (
-
-          <>
+        <button type="button" onClick={handleLogout}>
+          Logout
+        </button>
+        <button><Link to="/">Home</Link></button>
+        <button><Link to="/announcements">Announcements</Link></button>
+        <button><Link to="/discussion">Discussion</Link></button>
+        <button><Link to="/">Home</Link></button>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/Register' element={<Register />} />
+          <Route element={<ProtectedLayout />}>
+            <Route path="/" element={<Lectures />} />
+            <Route path="/announcements" element={<Announcements />} />
+            <Route path="/discussion" element={<GeneralDiscussion />} />
+          </Route>
+          <Route path="*" element={<h1>Oops, not found!</h1>} />
+        </Routes>
+          {/* <>
           <p>Hello: {name}</p>
           <button type="button" onClick={() => dispatch(toggleName())}>
             Toggle name
           </button>
           <div>
-            <button type="button" onClick={handleLogout}>
-              Logout
-            </button>
+
           </div>
-        </>
-      )}
+        </> */}
       <Toaster reverseOrder={true} />
       </header>
     </div>
