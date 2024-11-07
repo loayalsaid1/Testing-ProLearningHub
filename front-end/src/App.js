ed0f3214a6e14b69a9dd6d2df3837d3f435eee75
Loay Al-Said <l.programming555@gmail.com>
2024-11-07 13:01:15 +0200

    Prepare for routing.. clean App.js mess

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState }from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
 import './App.css';
@@ -7,36 +7,19 @@ import { logout } from './redux/actions/uiActionCreators';
 import Spinner from './components/utilityComponents/Spinner';
 import { googleLogout } from '@react-oauth/google';
 import Authintication from './components/Authintication/Authintication';
-// import Lecture from './components/Lecture/Lecture';
-// import TextEditor from './components/TextEditor/TextEditor';
-// import LectureDiscussion from './components/Discussion/LectureDiscussion';
-// import GeneralDiscussion from './components/GeneralDiscussion/GeneralDiscussion';
-// import Announcements from './components/Announcements/Announcements';
-import Announcements from './components/Announcements/Announcements';
-
 
 function App() {
   const name = useSelector((state) => state.hello.get('name'));
   const isLoading = useSelector((state) => state.ui.get('isLoading'));
-  // const isLoggedIn = useSelector((state) => state.ui.get('isLoggedIn'));
-  const isLoggedIn = true;
+  const isLoggedIn = useSelector((state) => state.ui.get('isLoggedIn'));
   const dispatch = useDispatch();
 
   const handleLogout = () => {
     dispatch(logout());
     googleLogout();
-  }
-  const [view, setView]  = useState('dashboard'); 
-  const [value, setValue] = useState('');
-  const [files, setFiles] = useState([]);
-  
-  /**
-   * This is messy nwo because... I'm not making the routing now.. 
-   * i'm making individual views for now first.. and this is my test field. now
-   * It's time is coming
-   * 
-   * BTW.. I'm not sure this approach is the best or not
-   */
+  };
+
+
   return (
     <div className="APP">
       {isLoading && <Spinner />}
@@ -44,42 +27,20 @@ function App() {
         {!isLoggedIn ? (
           <Authintication />
         ) : (
+
           <>
-            {view === 'dashboard' ? (
-              <>
-                <p>Hello: {name}</p>
-                <button type="button" onClick={() => dispatch(toggleName())}>
-                  Toggle name
-                </button>
-                <div>
-                  <button type="button" onClick={handleLogout}>
-                    Logout
-                  </button>
-                </div>
-                <div>
-                  <button type="button" onClick={() => setView('sections')}>
-                    Show sections
-                  </button>
-                </div>
-              </>
-            ) : (
-              <>
-                {/* <TextEditor value={value} setValue={setValue} files={files} setFiles={setFiles}  />
-                <button type="button" onClick={() => console.log(value, files)}>Check dataa</button> */}
-                {/* <Lecture lectureId='testId' /> */}
-                {/* <GeneralDiscussion courseId='testId' /> */}
-                <Announcements />
-                <button type="button" onClick={() => setView('dashboard')}>
-                  Go to dashboard
-                </button>
-                {/* <Lectures /> */}
-              </>
-            )}
-
-          </>
-        )}
-
-        <Toaster reverseOrder={true} />
+          <p>Hello: {name}</p>
+          <button type="button" onClick={() => dispatch(toggleName())}>
+            Toggle name
+          </button>
+          <div>
+            <button type="button" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+        </>
+      )}
+      <Toaster reverseOrder={true} />
       </header>
     </div>
   );
