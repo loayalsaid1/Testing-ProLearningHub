81d6324d6f951aa41234c139a92f0802389d73ee
loayalsaid1 <92930196+loayalsaid1@users.noreply.github.com>
2024-11-13 18:03:05 +0200

    Fix the typo (#24)

--- front-end/src/components/DiscussionEntries/DiscussionEntry.jsx
+++ front-end/src/components/DiscussionEntries/DiscussionEntry.jsx
@@ -101,7 +101,7 @@ export default function DiscussionEntry({ content, isLecture }) {
             to={`/questions/${content.get('id')}`}
             state={{ backRoute: window.location.pathname, isLecture }}
           >
-            {content.get('commentsCount')} <MessagesSquare />
+            {content.get('repliesCount')} <MessagesSquare />
           </Link>
         </button>
       </div>
