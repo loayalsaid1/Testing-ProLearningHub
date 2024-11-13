b2012793948a7fd4d01a2e0ee863e8c1b0d13960
Loay Al-Said <l.programming555@gmail.com>
2024-11-13 13:38:53 +0200

    Connect upvotes of questions to Redux

--- front-end/src/components/Replies/QuestionHeader.jsx
+++ front-end/src/components/Replies/QuestionHeader.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Dot, CircleArrowUp, EllipsisVertical } from 'lucide-react';
 import { formatDate } from '../../utils/utilFunctions';
+import {
+  makeGeneralQuestionIsUpvotedSelector,
+  makeGeneralQuestionUpvotesSelector,
+  makeLectureQuestionIsUpvotedSelector,
+  makeLectureQuestionUpvotesSelector,
+} from '../../redux/selectors/DiscussionsSelectors';
+import { toggleDiscussionEntryVote } from '../../redux/actions/discussionsThunks';
+
 
 export default function QuestionHeader({ question, isLecture }) {
+  let upvoted;
+  let upvotes;
+  const lectureId = question.get('lectureId');
+  console.log(lectureId);
+  upvoted = useSelector(
+    isLecture
+      ? makeLectureQuestionIsUpvotedSelector(lectureId, question.get('id'))
+      : makeGeneralQuestionIsUpvotedSelector(question.get('id'))
+  );
+  upvotes = useSelector(
+    isLecture
+      ? makeLectureQuestionUpvotesSelector(lectureId, question.get('id'))
+      : makeGeneralQuestionUpvotesSelector(question.get('id'))
+  );
+
+  console.log(upvoted, upvotes);
+  const date = formatDate(question.get('updatedAt'));
+
+  const dispatch = useDispatch();
+  const toggleUpvote = () => {
+    dispatch(
+      toggleDiscussionEntryVote(
+        question.get('id'),
+        isLecture,
+        question.get('lectureId')
+      ) || undefined
+    );
+  };
+
   return (
     <>
       <div>
@@ -20,9 +58,13 @@ export default function QuestionHeader({ question, isLecture }) {
         <div>{question.get('body')}</div>
       </div>
       <div>
-        <button type="button">
-          {question.get('upvotes')}{' '}
-          <CircleArrowUp color="grey" strokeWidth={2} />
+        <button type="button" onClick={toggleUpvote}>
+          {upvotes}{' '}
+          {!upvoted ? (
+            <CircleArrowUp color="grey" strokeWidth={2} />
+          ) : (
+            <CircleArrowUp color="black" strokeWidth={2.2} />
+          )}
         </button>
         <button
           type="button"
