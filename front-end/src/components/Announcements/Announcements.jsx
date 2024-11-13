f978aa807578a7490a1e535aa4ff103a46a27d79
Loay Al-Said <l.programming555@gmail.com>
2024-11-13 03:05:27 +0200

    Add ui functionality to add new announcement

--- front-end/src/components/Announcements/Announcements.jsx
+++ front-end/src/components/Announcements/Announcements.jsx
@@ -1,13 +1,18 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import Loading from '../utilityComponents/Loading';
 import AnnouncementEntry from './AnnouncementEntry';
+import { CirclePlus } from 'lucide-react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectIsLoading, selectAnnouncements } from '../../redux/selectors/announcementsSelectors';
+import { selectUserRole } from '../../redux/selectors/uiSelectors';
 import { fetchAnnouncements } from '../../redux/actions/announcementsThunks';
+import DiscussionEntryEditor from '../DiscussionEntries/DiscussionEntryEditor';
 
 export default function Announcements() {
+	const [showAnnouncementsEditor, setShowAnnouncementsEditor] = useState(false);
 		const isLoading = useSelector(selectIsLoading);
 		const announcements = useSelector(selectAnnouncements);
+		const userRole = useSelector(selectUserRole);
 
 		const dispatch = useDispatch();
 		useEffect(() => {
@@ -15,23 +20,36 @@ export default function Announcements() {
 				dispatch(fetchAnnouncements());
 		}, [dispatch, announcements])
 
+		const createNewAnnouncement = (title, details) => {
+			setShowAnnouncementsEditor(false);
+			console.log(title)
+			console.log(details);
+		}
+
 
 		return (
 			<div>
-			<h1>Announcements</h1>
-			{
-				isLoading ? (
+				<h1>Announcements</h1>
+				{isLoading ? (
 					<Loading />
+				) : announcements.size === 0 ? (
+					<h1>No announcements yet</h1>
 				) : (
-					announcements.size === 0 ? (
-						<h1>No announcements yet</h1>
-					) : (
-						announcements.map((announcement) => (
-							<AnnouncementEntry key={announcement.get('id')} content={announcement} />
-						))
-					)
-				)
-			}
-		</div>
-	);
+					announcements.map((announcement) => (
+						<AnnouncementEntry key={announcement.get('id')} content={announcement} />
+					))
+				)}
+				{userRole !== 'student' && (
+					<>
+						{!showAnnouncementsEditor ? (
+							<button type="button" onClick={() => setShowAnnouncementsEditor(true)}>
+								<CirclePlus size={48}/>
+							</button>
+						) : (
+							<DiscussionEntryEditor onPublish={createNewAnnouncement} />
+						)}
+					</>
+				)}
+			</div>
+		);
 }
